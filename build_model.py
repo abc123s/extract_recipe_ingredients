66de9fe5d63d32624fd09412b61eb29a15e5fcbc
Peter Zhang <peter0zhang@gmail.com>
2020-05-24 21:23:56 -0700

    modify train script to run simple experiments for basic architecture search

---
+++ build_model.py
@@ -0,0 +1,46 @@
+from tensorflow import keras
+
+def build_model(architecture, vocab_size, tag_size):
+    if architecture == 'rnn':
+        return keras.Sequential([
+            keras.layers.Embedding(vocab_size, 64, mask_zero = True),
+            keras.layers.SimpleRNN(64, return_sequences=True),
+            keras.layers.Dense(tag_size),
+        ])
+
+    if architecture == 'birnn':
+        return keras.Sequential([
+            keras.layers.Embedding(vocab_size, 64, mask_zero = True),
+            keras.layers.Bidirectional(keras.layers.SimpleRNN(64, return_sequences=True)),
+            keras.layers.Dense(tag_size),
+        ])
+
+    if architecture == 'gru':
+        return keras.Sequential([
+            keras.layers.Embedding(vocab_size, 64, mask_zero = True),
+            keras.layers.GRU(64, return_sequences=True),
+            keras.layers.Dense(tag_size),
+        ])
+
+    if architecture == 'bgru':
+        return keras.Sequential([
+            keras.layers.Embedding(vocab_size, 64, mask_zero = True),
+            keras.layers.Bidirectional(keras.layers.GRU(64, return_sequences=True)),
+            keras.layers.Dense(tag_size),
+        ])
+
+    if architecture == 'lstm':
+        return keras.Sequential([
+            keras.layers.Embedding(vocab_size, 64, mask_zero = True),
+            keras.layers.LSTM(64, return_sequences=True),
+            keras.layers.Dense(tag_size),
+        ])
+
+    if architecture == 'blstm':
+        return keras.Sequential([
+            keras.layers.Embedding(vocab_size, 64, mask_zero = True),
+            keras.layers.Bidirectional(keras.layers.LSTM(64, return_sequences=True)),
+            keras.layers.Dense(tag_size),
+        ])
+    
+    
--- train.py
+++ train.py
@@ -1,55 +1,56 @@
-import tensorflow as tf
-from tensorflow import keras
+import os
+from datetime import datetime
+import subprocess
 
 import numpy as np
 
+import tensorflow as tf
+from tensorflow import keras
+
 from preprocess import preprocess
+from build_model import build_model
 from masked_accuracy import SparseCategoricalAccuracyMaskZeros
+
+TRAIN_SIZE = 5000
 
 train_data, dev_data, _, word_encoder, tag_encoder = preprocess("./data")
 BUFFER_SIZE = 1000
 train_batches = (train_data
-    .take(5000)
+    .take(TRAIN_SIZE)
     .shuffle(BUFFER_SIZE)
     .padded_batch(32, padded_shapes = ([None], [None]))
 )
 
-dev_batches = dev_data.take(1000).padded_batch(32, padded_shapes = ([None], [None]))
+dev_batches = dev_data.padded_batch(32, padded_shapes = ([None], [None]))
 
-model = keras.Sequential([
-    keras.layers.Embedding(word_encoder.vocab_size, 64, mask_zero = True),
-    keras.layers.LSTM(64, return_sequences=True),
-    keras.layers.Dense(tag_encoder.vocab_size),
-])
+# build model:
+ARCHITECTURE = "rnn"
+model = build_model(ARCHITECTURE, word_encoder.vocab_size, tag_encoder.vocab_size)
 
-model.summary()
-
+# compile model:
+OPTIMIZER = "adam"
 model.compile(
-    optimizer="adam",
+    optimizer=OPTIMIZER,
     loss=tf.losses.SparseCategoricalCrossentropy(from_logits = True),
-    metrics=[SparseCategoricalAccuracyMaskZeros(name = "masked_accuracy"), 'accuracy']
+    metrics=[SparseCategoricalAccuracyMaskZeros(name = "accuracy")]
 )
 
+# fit model:
+EPOCHS = 10
 history = model.fit(
     train_batches,
-    epochs=10,
+    epochs = EPOCHS,
     validation_data=dev_batches,
     validation_steps=30,
 )
 
-loss, masked_accuracy, accuracy = model.evaluate(dev_batches)
+# evaluate model:
 
-print("Loss: ", loss)
-print("Masked Accuracy: ", masked_accuracy)
-print("Accuracy: ", accuracy)
+# Tag-level accuracy and loss
+loss, tag_accuracy = model.evaluate(dev_batches)
 
-predict_model = keras.Sequential([
-    model,
-    keras.layers.Softmax(),
-])
 
-tag_correct = 0
-tag_total = 0
+# Sentence-level accuracy
 sentence_correct = 0
 sentence_total = 0
 for dev_example, dev_label in dev_data.take(1000):
@@ -61,32 +62,37 @@ for dev_example, dev_label in dev_data.take(1000):
     answer = dev_label.numpy().tolist()
 
     correct = [pred == label for pred, label in zip(prediction, answer)]
-    tag_correct += sum(correct)
-    tag_total += len(correct)
 
     sentence_total += 1
     if len(correct) == sum(correct):
         sentence_correct += 1
-    
-    if sentence_total % 100 == 0:
-        print('')
-        print(word_encoder.decode(dev_example))
-        print(
-            tag_encoder.decode(keras.backend.flatten(
-                keras.backend.argmax(
-                    model(np.array([dev_example]))
-                )
-            ))
-        )
-        print(tag_encoder.decode(dev_label))
-        print('summary stats:')
-        print(tag_correct)
-        print(tag_total)
-        print(sentence_correct)
-        print(sentence_total)
 
-print('Sentence accuracy')
-print(sentence_correct / sentence_total)
-print('Tag accuracy')
-print(tag_correct / tag_total)
+sentence_accuracy = sentence_correct / sentence_total
 
+# save experiment results down
+date_string = datetime.now().strftime("%Y%m%d_%H%M")
+commit_string = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).strip().decode("utf-8")
+experiment_dir = "experiments/" + date_string + "_" + commit_string
+os.mkdir(experiment_dir)
+
+# save model weights for later usage
+model.save_weights(experiment_dir + "/model_weights")
+
+# save params and results
+f = open(experiment_dir + "/results.txt", "w")
+
+f.write("Params\n")
+f.write("ARCHITECTURE: " + ARCHITECTURE + "\n")
+f.write("OPTIMIZER: " + OPTIMIZER + "\n")
+f.write("EPOCHS: " + str(EPOCHS) + "\n")
+f.write("TRAIN_SIZE: " + str(TRAIN_SIZE) + "\n\n")
+
+f.write("Sentence-Level Stats:" + "\n")
+f.write("\tcorrect: " + str(sentence_correct) + "\n")
+f.write("\ttotal: " + str(sentence_total) + "\n")
+f.write("\t% correct: " + str(100 * sentence_accuracy) + "\n\n")
+
+f.write("Word-Level Stats:\n")
+f.write("\t% correct:" + str(100 * tag_accuracy) + "\n")
+
+f.close()
